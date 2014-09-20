befae2f346bcea240e2f47b00f5a43bb0b20cfd2
Michal Wszolek <miwszole@microsoft.com>
2014-09-20 13:31:45 +0200

    version before bootstrap and styling

--- 2_angular/js/app.js
+++ 2_angular/js/app.js
@@ -1,31 +1,27 @@
 // Michal Wszolek - basket demo app - AngularJS
 
-function BasketCtrl($scope) {
-  var products = [
-    { name: "smartphone", quantity: 0, unit_price: 200 },
-    { name: "tablet", quantity: 0, unit_price: 350 },
-    { name: "netbook", quantity: 0, unit_price: 400 },
-    { name: "laptop", quantity: 0, unit_price: 1000 }
-  ];
+var products = [
+  { name: "smartphone", quantity: 0, unit_price: 200 },
+  { name: "tablet", quantity: 0, unit_price: 350 },
+  { name: "netbook", quantity: 0, unit_price: 400 },
+  { name: "laptop", quantity: 0, unit_price: 1000 }
+];
 
+angular.module('Basket', []).controller('BasketController', function($scope) { 
   $scope.offer = products;
 
-  $scope.selectedFilter = function (location) {
-    return location.quantity > 0;
+  $scope.selectedFilter = function (product) {
+    return product.quantity > 0;
   };
 
   $scope.getTotalPrice = function () {
-    var sum = 0;
-    $scope.offer.forEach( function(item, idx) {
-      sum += item.quantity * item.unit_price;
-    });
-    return sum;
+    return $scope.offer.map(function(a){return a.quantity*a.unit_price;}).reduce(function(a,b) {return a+b;});
   };
 
   $scope.addToBasket = function(id) {
     var additional_quantity = parseInt($scope.offer[id].input_value);
-    if(!isNaN(additional_quantity)) {
+    if(!isNaN(additional_quantity) && additional_quantity>0) {
       $scope.offer[id].quantity += additional_quantity;
     }
   }
-}
+});
